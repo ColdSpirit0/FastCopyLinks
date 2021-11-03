4cef014d0d33cde2f27b6d03fbf3096438137df2
xanoni <77220130+xanoni@users.noreply.github.com>
2021-11-02 21:19:31 -0400

    Update pack.sh for MacOS

--- pack.sh
+++ pack.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
+# please set path to Firefox executable
 BROWSER_PATH=""
+
 EXTENSION_NAME="fast_copy_links.xpi"
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 # remove previous extension
-rm -f $EXTENSION_NAME
+rm -f ${EXTENSION_NAME}
 
 # pack
-cd extension
-zip -r "../$EXTENSION_NAME" *
+cd "${SCRIPT_DIR}/extension" || exit 1
+zip -r "../${EXTENSION_NAME}" ./*
 cd ..
 
 # if browser path exists
-if [ -n "$BROWSER_PATH" ]
+if [ -n "${BROWSER_PATH}" ]
 then
 	# install plugin
-	start "$BROWSER_PATH" "$EXTENSION_NAME"
+	if   [[ "${OSTYPE}" = linux* ]]; then
+		start "${BROWSER_PATH}" "${EXTENSION_NAME}"
+	elif [[ "${OSTYPE}" = darwin20* ]]; then
+		# this may only work if browser is currently not open
+		open "${BROWSER_PATH}" --args "${SCRIPT_DIR}/${EXTENSION_NAME}"
+	else
+		echo "Please install ${EXTENSION_NAME} manually."
+	fi
 fi
